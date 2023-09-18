57ad05775ef1fae3bfa51f69f2f95770bb783042
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-09-18 14:09:19 -0700

    play again ft, added comments

--- hangman.py
+++ hangman.py
@@ -9,21 +9,26 @@ def game():
     while (tries < 0):
         cur_guess = input("Enter a character in the alphabet: ")
         guess_num(cur_guess, tries, guessed, cur_word, hidden)
-    ## helper function for taking a guess
-    ## ask for letter input
-    ## if not a letter, ask for another input
-    ## else if go through characters of cur_word and "reveal" letters w/ print statement
-    ## if guess is wrong then -= from tries counter
+        if (hidden==cur_word):
+            print("Congratulations! You guessed the word!")
+            return
+    print("Game over! You have run out of tries...")
+    repeat = input("Play again? (Y/N): ")
+    if (repeat.lower=="y"):
+        game()
+
 
 def guess_num(cur_guess, tries, guessed, cur_word, hidden):
-    if len(cur_guess)==1 and cur_guess.isalpha() and cur_guess not in guessed:
-        if (cur_guess in cur_word):
+    if len(cur_guess)==1 and cur_guess.isalpha() and cur_guess not in guessed: ## if guess is valid
+        if (cur_guess in cur_word): ## correct guess, reveals places in which guessed num are in word
             for i in range(len(cur_word)):
                 if cur_word[i] == cur_guess:
                     hidden[i] = cur_guess
-        else:
+            print("Current word: ", hidden)
+            guessed.append(cur_guess)
+        else: ## wrong guess, tries--
             tries -= 1
-    else:
+    else: # gets another input
         cur_guess = input("Guess invalid, enter a character in the alphabet: ")
         guess_num(cur_guess, tries, guessed, cur_word)
 
